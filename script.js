cb6d2166b86541e87b3878bb645d31eada93bbb4
JarosPiotr <jarospiotrweb@gmail.com>
2020-10-11 14:53:34 +0200

    with match list

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const press = document.querySelector('input')
 const icon = document.querySelector('.cityName i')
 const startPage = document.querySelector('.startPageOn')
 const loadingBar = document.querySelector('.loading0')
+const matchList = document.querySelector('.matchListOff')
 const weekday = [
   'Sunday',
   'Monday',
@@ -32,21 +33,21 @@ let weekdaySchedule = [];
 let citySearched;
 let cityData;
 let weatherData;
-
+let selectedCity;
 
 
 //API
-async function getCityLatLon() {
-  try{
-  const response = await fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=1&namePrefix=${citySearched}`, {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
-      "x-rapidapi-key": "f4a11bf4cbmsh5b92adb0e60713cp1d13b1jsn9bba4dd46055"
-    }
-  })
-  cityData = await response.json();
-} catch { console.log('cant respoine')}
+async function getCityLatLon(x) {
+  try {
+    const response = await fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=5&namePrefix=${x}`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+        "x-rapidapi-key": "f4a11bf4cbmsh5b92adb0e60713cp1d13b1jsn9bba4dd46055"
+      }
+    })
+    cityData = await response.json();
+  } catch { console.log('cant respoine') }
 
 }
 
@@ -61,12 +62,12 @@ async function getWeather() {
 //API
 
 const cityDetails = document.querySelector('.fetchedCityDetails')
-const flag =document.querySelector('.cntFlag')
+const flag = document.querySelector('.cntFlag')
 const cityTab = document.querySelector('.cityTab')
-const dayTabs= document.querySelector('.dayTabs')
+const dayTabs = document.querySelector('.dayTabs')
 const cityInfo = document.querySelector('.cityInfo')
 const errorTab = document.querySelector('.errorTabOff')
-let newtable=[];
+let newtable = [];
 
 setTodayDate()
 setDaysOrder()
@@ -74,56 +75,74 @@ setWeather()
 
 
 citySearched =
-  press.addEventListener('input', () => {    
+  press.addEventListener('input', () => {
     icon.classList.value = "fas fa-check-circle"
   })
 
 icon.addEventListener('click', () => {
-  if(press.value == "") {
+  if (press.value == "") {
 
-  }else{
-  citySearched = document.querySelector('input').value
-  console.log(citySearched)
-  getCityLatLon()
-  console.log(cityData)
-  loadingBar.classList.value = 'loading1'
-  setTimeout(()=> {if(cityData.data.length == 0){
-    errorTab.classList.value = 'errorTabOn'
-    setTimeout(()=> {
-      errorTab.classList.value = 'errorTabOff'
-      press.value = ""
-      icon.classList.value = "fas fa-map-marker-alt"
-      loadingBar.classList.value = 'loading0'
-      press.focus()
-    }, 800)
-  } else { 
-  cityTab.classList.value = 'cityTabChanged'
-  cityInfo.classList.value = 'cityInfoChanged'
-  startPage.classList.value = 'startPageOff'
-  
+  } else {
+    citySearched = document.querySelector('input').value
+    console.log(citySearched)
+    getCityLatLon(citySearched)
+   console.log(cityData)
 
-  setTimeout(() => {  
-  cityDetails.innerHTML = `
-            <span class="">${displayName(cityData.data[0].city)}</span>
-            <span class="">${displayName(cityData.data[0].country)}</span>
-            <span class="">${cityData.data[0].latitude.toFixed(2)}</span>
-            <span class="">${cityData.data[0].longitude.toFixed(2)}</span>
-  `
-flag.innerHTML =`
-<img src="https://www.countryflags.io/${cityData.data[0].countryCode}/shiny/64.png">
-`
-  }, 800)
+   setTimeout(()=>{ if(cityData.data.length == 0 ){
+    wrongCityName()
+          } else {
+            letSelectCity() 
+          
+           
+          }
+          },450)
+          
+        }
+      }
+)
+               
+            
 
-  setTimeout(getWeather, 400)
-
-  setTimeout(weatherValues, 1000)
-  setTimeout(daysUpdate, 1000)
-  loadingBar.classList.value = 'loading0'
-}}, 2000)
+    
 
 
-  }
-})
+    //   setTimeout(()=> {if(cityData.data.length == 0){
+    //     errorTab.classList.value = 'errorTabOn'
+    //     setTimeout(()=> {
+    //       errorTab.classList.value = 'errorTabOff'
+    //       press.value = ""
+    //       icon.classList.value = "fas fa-map-marker-alt"
+    //       loadingBar.classList.value = 'loading0'
+    //       press.focus()
+    //     }, 800)
+    //   } else { 
+    //   cityTab.classList.value = 'cityTabChanged'
+    //   cityInfo.classList.value = 'cityInfoChanged'
+    //   startPage.classList.value = 'startPageOff'
+
+
+    //   setTimeout(() => {  
+    //   cityDetails.innerHTML = `
+    //             <span class="">${displayName(cityData.data[0].city)}</span>
+    //             <span class="">${displayName(cityData.data[0].country)}</span>
+    //             <span class="">${cityData.data[0].latitude.toFixed(2)}</span>
+    //             <span class="">${cityData.data[0].longitude.toFixed(2)}</span>
+    //   `
+    // flag.innerHTML =`
+    // <img src="https://www.countryflags.io/${cityData.data[0].countryCode}/shiny/64.png">
+    // `
+    //   }, 800)
+
+    //   setTimeout(getWeather, 400)
+
+    //   setTimeout(weatherValues, 1000)
+    //   setTimeout(daysUpdate, 1000)
+    //   loadingBar.classList.value = 'loading0'
+    // }}, 2000)
+
+
+//   }
+// })
 
 
 x.forEach((item, index) => {
@@ -147,16 +166,16 @@ function rollDown(index) {
   console.log("rollDown");
   z[index].style.visibility = "visible";
   z[index].style.height = "200px";
-  setTimeout(() => { 
-  z[index].children[0].style.opacity = 1;
-  z[index].children[1].style.opacity = 1;
- }, 400);
+  setTimeout(() => {
+    z[index].children[0].style.opacity = 1;
+    z[index].children[1].style.opacity = 1;
+  }, 400);
 
 }
 
 function rollUp() {
   z.forEach((item) => {
-    
+
     item.children[0].style.opacity = 0;
     item.children[1].style.opacity = 0;
     setTimeout(() => {
@@ -214,14 +233,14 @@ function daysUpdate() {
     item.children[2].innerHTML = `${Math.round(newtable[index].day)}<sup class="degC">&#8451;</sup>`
   })
   z.forEach((item, index) => {
-    item.querySelector('.dayInfoEveningValue').textContent = 
-    newtable[index].eve
-    item.querySelector('.dayInfoMorningValue').textContent = 
-    newtable[index].mor
-    item.querySelector('.dayInfoNightValue').textContent = 
-    newtable[index].night
-    item.querySelector('.dayInfoHumdityValue').textContent = 
-    newtable[index].hum
+    item.querySelector('.dayInfoEveningValue').textContent =
+      newtable[index].eve
+    item.querySelector('.dayInfoMorningValue').textContent =
+      newtable[index].mor
+    item.querySelector('.dayInfoNightValue').textContent =
+      newtable[index].night
+    item.querySelector('.dayInfoHumdityValue').textContent =
+      newtable[index].hum
 
   })
 }
@@ -236,40 +255,110 @@ function setTodayDate() {
 
 }
 
-function displayName(name){
-  
-  if(name.length >=13){
-    name = name.replace(/[a-z]/g,'')
-  } 
-  
+function displayName(name) {
+
+  if (name.length >= 13) {
+    name = name.replace(/[a-z]/g, '')
+  }
+
   return name
 }
 
-function setWeather(){
-  newtable = weekdaySchedule.map((item)=>{
-     item = {
+function setWeather() {
+  newtable = weekdaySchedule.map((item) => {
+    item = {
       name: item,
-      day:"",
-      eve:"",
-      mor:"",
-      night:"",
-      hum:"",
-      cond:""
- 
-      
+      day: "",
+      eve: "",
+      mor: "",
+      night: "",
+      hum: "",
+      cond: ""
+
+
     }
-  return item;
+    return item;
   })
 
-  
-  }
 
-  function weatherValues(){
-    newtable.forEach((item, index)=>{
-       item.day = weatherData[index].temp.day
-       item.eve = weatherData[index].temp.eve
-       item.mor = weatherData[index].temp.morn
-       item.night = weatherData[index].temp.night
-       item.hum = weatherData[index].humidity
+}
+
+function weatherValues() {
+  newtable.forEach((item, index) => {
+    item.day = weatherData[index].temp.day
+    item.eve = weatherData[index].temp.eve
+    item.mor = weatherData[index].temp.morn
+    item.night = weatherData[index].temp.night
+    item.hum = weatherData[index].humidity
+  })
+}
+
+function wrongCityName(){
+  errorTab.classList.value = 'errorTabOn'
+          setTimeout(()=> {
+            errorTab.classList.value = 'errorTabOff'
+            press.value = ""
+            icon.classList.value = "fas fa-map-marker-alt"
+            loadingBar.classList.value = 'loading0'
+            press.focus()
+          },500)
+}
+
+function letSelectCity() {
+  setTimeout(() => {
+    matchList.classList.value = "matchListOn"
+    let matchTable = cityData.data.map((item, index) => {
+      let matchItem = `<li><span class="${index}">${item.city}
+      </span>, ${
+        item.country
+      }</li>`
+
+      return matchItem
+    }
+
+    )
+    let matchTableLi = matchTable.join("")
+    matchList.innerHTML = `<ul>${matchTableLi}</ul>`
+
+    let matchListGet = [...document.querySelectorAll('.matchListOn span')]
+    matchListGet.forEach((item) => {
+      console.log(item.innerHTML)
     })
-  }
+
+
+    matchListGet.forEach((item, index) => {
+      item.addEventListener('click', () => {
+        console.log(item.textContent)
+        selectedCity = item.textContent
+         getCityLatLon(selectedCity)
+        setTimeout(() => {
+          console.log(cityData)
+          getWeather()
+          setTimeout(()=>{
+          matchList.innerHTML = ""
+          matchList.classList.value = "matchListOff"
+          console.log(weatherData)
+            cityTab.classList.value = 'cityTabChanged'
+            cityInfo.classList.value = 'cityInfoChanged'
+            startPage.classList.value = 'startPageOff'
+  
+      cityDetails.innerHTML = `
+                <span class="">${displayName(cityData.data[0].city)}</span>
+                <span class="">${displayName(cityData.data[0].country)}</span>
+                <span class="">${cityData.data[0].latitude.toFixed(2)}</span>
+                <span class="">${cityData.data[0].longitude.toFixed(2)}</span>
+      `
+    flag.innerHTML =`
+    <img src="https://www.countryflags.io/${cityData.data[0].countryCode}/shiny/64.png">
+    `
+         setTimeout(getWeather, 400)
+
+      setTimeout(weatherValues, 1000)
+      setTimeout(daysUpdate, 1000)
+
+          },500)
+        }, 500)
+      })
+    })
+  }, 1000)
+}
